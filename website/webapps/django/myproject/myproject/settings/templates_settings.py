a37df95026ecc075334490891723f01b6e932194
Martin Brochhaus <mbrochh@gmail.com>
2012-03-31 02:57:31 +0800

    Removed django-cms template processors

--- website/webapps/django/myproject/myproject/settings/templates_settings.py
+++ website/webapps/django/myproject/myproject/settings/templates_settings.py
@@ -13,13 +13,11 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.media',
     'django.core.context_processors.static',
     'django.contrib.messages.context_processors.messages',
-    'cms.context_processors.media',
-    'sekizai.context_processors.sekizai',
 )
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-    #'django.template.loaders.eggs.Loader',
+    'django.template.loaders.eggs.Loader',
 )
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_ROOT, 'templates'),
@@ -27,7 +25,6 @@ TEMPLATE_DIRS = (
 THUMBNAIL_PROCESSORS = (
     'easy_thumbnails.processors.colorspace',
     'easy_thumbnails.processors.autocrop',
-    #'easy_thumbnails.processors.scale_and_crop',
-    'filer.thumbnail_processors.scale_and_crop_with_subject_location',
+    'easy_thumbnails.processors.scale_and_crop',
     'easy_thumbnails.processors.filters',
 )
