908f150f9b29e34db545c388cd83cc9c26c71c1b
Martin Brochhaus <mbrochh@gmail.com>
2012-06-13 19:30:17 +0800

    Fixed bug in locale_backup script

--- scripts/locale-backup.sh
+++ scripts/locale-backup.sh
@@ -4,9 +4,13 @@ BACKUPFOLDER='backups_locale_'$PROJECTNAME
 mkdir -p $HOME/$BACKUPFOLDER
 
 KEEP=60
-ls -tr1 $HOME/$BACKUPFOLDER/locale-*.tgz | head -n 1 | xargs rm -f
+BACKUPS=`find $HOME/$BACKUPFOLDER -name "locale-*.tgz" | wc -l | sed 's/\ //g'`
+while [ $BACKUPS -ge $KEEP ]
+do
+ls -tr1 $HOME/$BACKUPFOLDER/locale-*.gz | head -n 1 | xargs rm -f
 BACKUPS=`expr $BACKUPS - 1`
 done
+
 DATE=`date +%Y%m%d%H%M%S`
 rm -f $HOME/$BACKUPFOLDER/.locale-${DATE}.tgz_INPROGRESS
 tar -cvzpf $HOME/$BACKUPFOLDER/.locale-${DATE}.tgz_INPROGRESS ~/webapps/$DJANGO_APP_NAME/myproject/locale
